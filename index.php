604f0c80aab7eedef1c787f7f57b8dbe734ee33e
iftekher sunny <iftekhersunny@hotmail.com>
2016-04-01 22:37:17 +0600

    Added comment

--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 
 include __DIR__ . '/vendor/autoload.php';
 
-$envato = new Sun\Envato('NPGEwrl7HDaVXwNjM2cHfM4rNKnhCrBZ');
+
+$envato = new Sun\Envato('NPGEwrl7HDaVXwNjM2cHfM4rNKnhCrBZ'); // add your envato personal token
 
 
-$purchase = $envato->verifyPurchaseCode('b67d10b4-3976-455b-85da-9f3fcc15226e');
+$purchase = $envato->verifyPurchaseCode('b67d10b4-3976-455b-85da-9f3fcc15226e'); // add your purchase code to verify sale
 
 if($purchase) echo 'Purchased';
 else echo 'Not purchased';
